7b9b8badc13b8b852039f7693f01c628609087f1
cmiklos-kh <cmiklos@kendallhunt.com>
2026-01-22 10:06:13 -0600

    Moved CSS to heleper

--- helpers.php
+++ helpers.php
@@ -9,3 +9,37 @@ function basePath($path = '')
 {
     return __DIR__ . '/' . $path;
 }
+
+/*
+*Load view
+*@param Sring $name
+*return void
+*/
+
+function loadView($name)
+{
+    $viewPath = basePath("views/{$name}.view.php");
+
+    if (file_exists($viewPath)) {
+        require $viewPath;
+    } else {
+        echo "View {$name} not found";
+    }
+}
+
+/* 
+*Load partial
+*@param String $name
+*return void
+*/
+
+function loadPartial($name)
+{
+    $partialPath = basePath("views/partials/{$name}.php");
+
+    if (file_exists($partialPath)) {
+        require $partialPath;
+    } else {
+        echo "View {$name} not found";
+    }
+}
